c772c93408edacc58071f2822385a1481a540184
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-04-26 16:28:37 +0400

    create form func

--- frontend/src/pages/Notfound.jsx
+++ frontend/src/pages/Notfound.jsx
@@ -1,6 +1,10 @@
 function NotFound(){
-    
-    return <div>NotFound</div>
+
+    return <div>
+        <h1>NotFound</h1>
+        <p>The page you are looking for doesn't exist</p>
+
+        </div>
     
 }
 export default NotFound
